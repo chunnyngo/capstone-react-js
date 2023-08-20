96c735870a1c272ba8a2268cab0518123bba98b4
Ngô Quang Trường <truongngo142000@gmail.com>
2023-08-20 21:00:05 +0700

    up

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -32,63 +32,63 @@ export default function Detail() {
     },
   ];
   return (
-    <div
-      style={{
-        backgroundImage: `url(${movie.hinhAnh})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <div>
       <div
-        style={{ paddingTop: "80px", minHeight: "100vh" }}
-        color="#DCDCDC" // default color is white
+        className="bg-indigo-300"
+        style={{
+          // backgroundImage: `url(${movie.hinhAnh})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <div className="grid grid-cols-12">
-          <div className="col-span-4 col-start-4">
-            <div className="grid grid-cols-2">
-              <img
-                src={movie.hinhAnh}
-                alt="thumb"
-                style={{
-                  width: 300,
-                  height: 400,
-                  borderRadius: "20px",
-                  objectFit: "cover",
-                }}
-              />
-              <div className="pl-5" style={{ marginTop: "60%" }}>
-                <div className="inline-block bg-violet-400 p-2 mb-3 text-white font-bold rounded-lg">
-                  Ngày Khởi Chiếu: {movie.ngayKhoiChieu}
+        <div style={{ paddingTop: "80px", minHeight: "100vh" }}>
+          <div className="grid grid-cols-12">
+            <div className="col-span-4 col-start-4">
+              <div className="grid grid-cols-2">
+                <img
+                  src={movie.hinhAnh}
+                  alt="thumb"
+                  style={{
+                    width: 300,
+                    height: 400,
+                    borderRadius: "20px",
+                    objectFit: "cover",
+                  }}
+                />
+                <div className="pl-5" style={{ marginTop: "60%" }}>
+                  <div className="inline-block bg-violet-400 p-2 mb-3 text-white font-bold rounded-lg">
+                    Ngày Khởi Chiếu: {movie.ngayKhoiChieu}
+                  </div>
+                  <h1 className="text-5xl font-bold text-white ">
+                    {movie.tenPhim}
+                  </h1>
                 </div>
-                <h1 className="text-5xl font-bold text-white ">
-                  {movie.tenPhim}
-                </h1>
               </div>
             </div>
+            <div className="p-20 ">
+              <Space wrap>
+                <Progress
+                  type="circle"
+                  percent={movie.danhGia * 10}
+                  strokeColor={"#a78bfa"}
+                  status="active"
+                  size={250}
+                  trailColor="white"
+                />
+                <div className="pl-14">
+                  <Rate value={movie.danhGia / 2} allowClear={false} />
+                </div>
+              </Space>
+            </div>
           </div>
-          <div className="p-20 ">
-            <Space wrap>
-              <Progress
-                type="circle"
-                percent={movie.danhGia * 10}
-                strokeColor={"#a78bfa"}
-                status="active"
-                size={250}
-                trailColor="white"
-              />
-              <div className="pl-14">
-                <Rate value={movie.danhGia / 2} allowClear={false} />
-              </div>
-            </Space>
+          <div className="containerFilm ">
+            <Tabs
+              items={items}
+              onChange={onChange}
+              className="text-white text-xl"
+            />
           </div>
-        </div>
-        <div className="containerFilm ">
-          <Tabs
-            items={items}
-            onChange={onChange}
-            className="text-white text-xl"
-          />
         </div>
       </div>
     </div>
